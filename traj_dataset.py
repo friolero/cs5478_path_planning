1dae3b261853e5f81df1bd1e83fd91c9d451d850
Zeng Yuwei <yuwei.zeng0101@gmail.com>
2023-11-22 04:23:59 +0000

    merge normalizer to dataset

--- traj_dataset.py
+++ traj_dataset.py
@@ -1,12 +1,114 @@
 import abc
 import os.path
 
-import git
+import einops
 import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-from normalizer import DatasetNormalizer
+
+def flatten(dataset):
+    """
+    flattens dataset of { key: [ ... x dim ] }
+        to { key : [ (...) x dim ]}
+    """
+    flattened = {}
+    for key, xs in dataset.items():
+        xs_new = xs
+        if xs.ndim == 2:
+            # environments (e d)
+            pass
+        elif xs.ndim == 3:
+            # trajectories in fixed environments
+            xs_new = einops.rearrange(xs, "b h d -> (b h) d")
+        elif xs.ndim == 4:
+            # trajectories in variable environments
+            xs_new = einops.rearrange(xs, "e b h d -> (e b h) d")
+        else:
+            raise NotImplementedError
+        flattened[key] = xs_new
+    return flattened
+
+
+class Normalizer:
+    """
+    parent class, subclass by defining the `normalize` and `unnormalize` methods
+    """
+
+    def __init__(self, X):
+        self.X = X
+        self.mins = X.min(dim=0).values
+        self.maxs = X.max(dim=0).values
+
+    def __repr__(self):
+        return (
+            f"""[ Normalizer ] dim: {self.mins.size}\n    -: """
+            f"""{torch.round(self.mins, decimals=2)}\n    +: {torch.round(self.maxs, decimals=2)}\n"""
+        )
+
+    def __call__(self, x):
+        return self.normalize(x)
+
+    def normalize(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def unnormalize(self, *args, **kwargs):
+        raise NotImplementedError()
+
+
+class LimitsNormalizer(Normalizer):
+    """
+    maps [ xmin, xmax ] to [ -1, 1 ]
+    """
+
+    def normalize(self, x):
+        # [ 0, 1 ]
+        x = (x - self.mins) / (self.maxs - self.mins)
+        # [ -1, 1 ]
+        x = 2 * x - 1
+        return x
+
+    def unnormalize(self, x, eps=1e-4):
+        """
+        x : [ -1, 1 ]
+        """
+        if x.max() > 1 + eps or x.min() < -1 - eps:
+            x = torch.clip(x, -1, 1)
+
+        # [ -1, 1 ] --> [ 0, 1 ]
+        x = (x + 1) / 2.0
+
+        return x * (self.maxs - self.mins) + self.mins
+
+
+class DatasetNormalizer:
+    def __init__(self, dataset, normalizer):
+        dataset = flatten(dataset)
+
+        if type(normalizer) == str:
+            normalizer = eval(normalizer)
+
+        self.normalizers = {}
+        for key, val in dataset.items():
+            self.normalizers[key] = normalizer(val)
+
+    def __repr__(self):
+        string = ""
+        for key, normalizer in self.normalizers.items():
+            string += f"{key}: {normalizer}]\n"
+        return string
+
+    def __call__(self, *args, **kwargs):
+        return self.normalize(*args, **kwargs)
+
+    def normalize(self, x, key):
+        return self.normalizers[key].normalize(x)
+
+    def unnormalize(self, x, key):
+        return self.normalizers[key].unnormalize(x)
+
+    def get_field_normalizers(self):
+        return self.normalizers
 
 
 class TrajectoryDatasetBase(Dataset):
